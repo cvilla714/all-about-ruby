c1eb911859f88a9266c3a276456b359225899ed9
Cosmel Villalobos <cosmelvg@gmail.com>
2021-08-19 21:05:31 -0400

    added new entries to test calculation

--- hashes5.rb
+++ hashes5.rb
@@ -4,9 +4,13 @@ user =
     arrival: '2021-11-26',
     departure: '2021-11-30',
     room_stays: [{ 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-26', 'external_code' => 'K1ERV', 'code' => nil },
+                 { 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-27',
+                   'external_code' => 'K1ERV', 'code' => nil },
                  { 'room_type_guid' => 'ae625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-30',
                    'external_code' => 'K1ERV', 'code' => nil },
                  { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-26',
+                   'external_code' => 'K1ERV1', 'code' => nil },
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-27',
                    'external_code' => 'K1ERV1', 'code' => nil },
                  { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-30',
                    'external_code' => 'K1ERV1', 'code' => nil }],
@@ -27,6 +31,8 @@ response_hilton =
      'amountAfterTax' => 596.44,
      'rateDetails' =>
      [{ 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-26', 'rate1Person' => 291.6, 'rate2Person' => 291.6, 'rate3Person' => 309.6, 'rate4Person' => 327.6 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-27', 'rate1Person' => 272.7, 'rate2Person' => 291.6,
+        'rate3Person' => 309.6, 'rate4Person' => 327.6 },
       { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-30', 'rate1Person' => 294.84, 'rate2Person' => 294.84,
         'rate3Person' => 313.04, 'rate4Person' => 331.24 }] },
    { 'roomTypeCode' => 'K1ERV1',
@@ -35,6 +41,8 @@ response_hilton =
      'amountAfterTax' => 915.0,
      'rateDetails' =>
      [{ 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-26', 'rate1Person' => 450.0, 'rate2Person' => 450.0, 'rate3Person' => 495.0, 'rate4Person' => 540.0 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-27', 'rate1Person' => 437.0, 'rate2Person' => 450.0,
+        'rate3Person' => 495.0, 'rate4Person' => 540.0 },
       { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-30', 'rate1Person' => 455.0, 'rate2Person' => 455.0,
         'rate3Person' => 500.5, 'rate4Person' => 546.0 }] }] }
 
@@ -44,11 +52,12 @@ response_hilton =
 # puts date_two
 # date_range = (date_one..date_two).to_a
 # puts date_range
-dates_ranges = (user[:arrival]..user[:departure]).to_a
-puts dates_ranges
-puts 'next line'
-puts dates_ranges[0]
-puts
+
+# dates_ranges = (user[:arrival]..user[:departure]).to_a
+# puts dates_ranges
+# puts 'next line'
+# puts dates_ranges[0]
+# puts
 
 # user[:room_stays].each do |lot|
 #   lot
@@ -379,13 +388,11 @@ codes = user[:room_stays].map do |code|
     rate['rateDetails'].each do |room_date|
       next unless room_date['effectiveDate'] == code['date']
 
-      luffy = room_date['rate1Person'].to_f * code['rooms_requested'].to_f
+      puts luffy = room_date['rate1Person'].to_f * code['rooms_requested'].to_f
       if total.has_key?(code['external_code'])
         total[code['external_code']] += luffy
       else total[code['external_code']] = luffy
       end
-      # luffy = room_date['rate1Person'].to_f * code['rooms_requested'].to_f
-      # puts luffy
     end
   end
 end
