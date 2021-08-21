67a5f88a88266ae45ab4252b7d879fd41c00ff90
Cosmel Villalobos <cosmelvg@gmail.com>
2021-08-20 22:27:57 -0400

    another solution

---
+++ hashes7.rb
@@ -0,0 +1,91 @@
+require 'date'
+
+user =
+  { hotel_code: 'NYCNH',
+    property_guid: '448ea118-2394-492f-ac87-b8b186f8eaf3',
+    arrival: '2021-11-26',
+    departure: '2021-11-30',
+    room_stays: [{ 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '5', 'date' => '2021-11-26', 'external_code' => 'K1ERV', 'code' => nil },
+                 { 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-27',
+                   'external_code' => 'K1ERV', 'code' => nil },
+                 { 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-28',
+                   'external_code' => 'K1ERV', 'code' => nil },
+                 { 'room_type_guid' => 'ae625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-30',
+                   'external_code' => 'K1ERV', 'code' => nil },
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-26',
+                   'external_code' => 'K1ERV1', 'code' => nil },
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-27',
+                   'external_code' => 'K1ERV1', 'code' => nil },
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-30',
+                   'external_code' => 'K1ERV1', 'code' => nil }],
+    number_of_guests: '1',
+    session_guid: 'e87b2b33-171b-4578-a698-9cbf17cc3587',
+    rate_code_override: 'GC36A',
+    peak_rooms: 20 }
+
+response_hilton =
+  { 'propCode' => 'NYCNH',
+    'ratePlanCode' => 'OG',
+    'serviceChargesAndTaxesIncluded' => false,
+    'currencyCode' => 'USD',
+    'roomRates' =>
+  [{ 'roomTypeCode' => 'K1ERV',
+     'inventory' => 12,
+     'totalServiceCharges' => 0.0,
+     'amountAfterTax' => 596.44,
+     'rateDetails' =>
+     [{ 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-26', 'rate1Person' => 291.6, 'rate2Person' => 291.6, 'rate3Person' => 309.6, 'rate4Person' => 327.6 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-27', 'rate1Person' => 272.7, 'rate2Person' => 291.6,
+        'rate3Person' => 309.6, 'rate4Person' => 327.6 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-30', 'rate1Person' => 294.84, 'rate2Person' => 294.84,
+        'rate3Person' => 313.04, 'rate4Person' => 331.24 }] },
+   { 'roomTypeCode' => 'K1ERV1',
+     'inventory' => 2,
+     'totalServiceCharges' => 0.0,
+     'amountAfterTax' => 915.0,
+     'rateDetails' =>
+     [{ 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-26', 'rate1Person' => 450.0, 'rate2Person' => 450.0, 'rate3Person' => 495.0, 'rate4Person' => 540.0 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-27', 'rate1Person' => 437.0, 'rate2Person' => 450.0,
+        'rate3Person' => 495.0, 'rate4Person' => 540.0 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-30', 'rate1Person' => 455.0, 'rate2Person' => 455.0,
+        'rate3Person' => 500.5, 'rate4Person' => 546.0 }] }] }
+
+total2 = {}
+user[:room_stays].map do |code|
+  code['external_code']
+  response_hilton['roomRates'].each do |rate|
+    next unless rate['roomTypeCode'] == code['external_code']
+
+    rate['rateDetails'].each do |room_date|
+      next unless room_date['effectiveDate'] == code['date']
+
+      luffy = room_date['rate1Person'].to_f * code['rooms_requested'].to_f
+
+      if !total2[code['external_code']]
+        total2[code['external_code']] == [{ dates: code['date'], sum: luffy }]
+      else
+        total2[code['external_code']].push({ dates: code['date'], sum: luffy })
+      end
+    end
+  end
+end
+
+puts total2
+
+# total2.keys.each do |_element|
+#   total2[code].each.with_index do |bola, index|
+#     puts bola[:dates]
+#     puts wats = index < total2.size - 1 ? total2[index + 1][:dates] : '1/1/1900'
+#     #   puts Date.parse(bola[:dates]) + 1
+#     if (Date.parse(bola[:dates]) + 1) == Date.parse(wats)
+#       puts bola[:sum] += total2[index + 1][:sum]
+#       total2.delete_at(index + 1)
+#     else
+#       puts 'not consecutive numbers'
+#       puts bola[:sum]
+#     end
+#     puts
+#   end
+# end
+
+# {"K1ERV": [{"arrival": "2021-11-26", "departure": "2021-11-28", "base_cost": 1458 + 2727}, {"arrival": "2021-11-30", "departure": "2021-11-31", "base_cost": 2948.39999}], "K1ERV1": [same thing for that room type]}
