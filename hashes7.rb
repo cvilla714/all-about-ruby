3c9a454d0964a347a23ac5775d2473d965db1d0d
Cosmel Villalobos <cosmelvg@gmail.com>
2021-08-22 15:59:48 -0400

    added more entries

--- hashes7.rb
+++ hashes7.rb
@@ -5,20 +5,28 @@ user =
     property_guid: '448ea118-2394-492f-ac87-b8b186f8eaf3',
     arrival: '2021-11-26',
     departure: '2021-11-30',
-    room_stays: [{ 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '5', 'date' => '2021-11-26', 'external_code' => 'K1ERV', 'code' => nil },
-                 { 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-27',
+    room_stays: [{ 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '5', 'date' => '2021-11-16', 'external_code' => 'K1ERV', 'code' => nil },
+                 { 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-17',
                    'external_code' => 'K1ERV', 'code' => nil },
-                 { 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-28',
+                 { 'room_type_guid' => 'a625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-18',
                    'external_code' => 'K1ERV', 'code' => nil },
-                 { 'room_type_guid' => 'ae625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-30',
+                 { 'room_type_guid' => 'ae625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-20',
                    'external_code' => 'K1ERV', 'code' => nil },
-                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-26',
+                 { 'room_type_guid' => 'ae625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-21',
+                   'external_code' => 'K1ERV', 'code' => nil },
+                 { 'room_type_guid' => 'ae625dce-248b-4be4-977d-abca7d3891c5', 'rooms_requested' => '10', 'date' => '2021-11-22',
+                   'external_code' => 'K1ERV', 'code' => nil },
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-16',
                    'external_code' => 'K1ERV1', 'code' => nil },
-                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-27',
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-17',
                    'external_code' => 'K1ERV1', 'code' => nil },
-                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-28',
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-18',
                    'external_code' => 'K1ERV1', 'code' => nil },
-                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-30',
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-20',
+                   'external_code' => 'K1ERV1', 'code' => nil },
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-21',
+                   'external_code' => 'K1ERV1', 'code' => nil },
+                 { 'room_type_guid' => '35d03fed-85fa-4de3-a6d8-16516735442b', 'rooms_requested' => '10', 'date' => '2021-11-22',
                    'external_code' => 'K1ERV1', 'code' => nil }],
     number_of_guests: '1',
     session_guid: 'e87b2b33-171b-4578-a698-9cbf17cc3587',
@@ -36,24 +44,32 @@ response_hilton =
      'totalServiceCharges' => 0.0,
      'amountAfterTax' => 596.44,
      'rateDetails' =>
-     [{ 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-26', 'rate1Person' => 291.6, 'rate2Person' => 291.6, 'rate3Person' => 309.6, 'rate4Person' => 327.6 },
-      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-27', 'rate1Person' => 272.7, 'rate2Person' => 291.6,
+     [{ 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-16', 'rate1Person' => 291.6, 'rate2Person' => 291.6, 'rate3Person' => 309.6, 'rate4Person' => 327.6 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-17', 'rate1Person' => 272.7, 'rate2Person' => 291.6,
         'rate3Person' => 309.6, 'rate4Person' => 327.6 },
-      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-28', 'rate1Person' => 282.7, 'rate2Person' => 291.6,
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-18', 'rate1Person' => 282.7, 'rate2Person' => 291.6,
         'rate3Person' => 309.6, 'rate4Person' => 327.6 },
-      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-30', 'rate1Person' => 294.84, 'rate2Person' => 294.84,
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-20', 'rate1Person' => 294.84, 'rate2Person' => 294.84,
+        'rate3Person' => 313.04, 'rate4Person' => 331.24 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-21', 'rate1Person' => 294.84, 'rate2Person' => 294.84,
+        'rate3Person' => 313.04, 'rate4Person' => 331.24 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-22', 'rate1Person' => 294.84, 'rate2Person' => 294.84,
         'rate3Person' => 313.04, 'rate4Person' => 331.24 }] },
    { 'roomTypeCode' => 'K1ERV1',
      'inventory' => 2,
      'totalServiceCharges' => 0.0,
      'amountAfterTax' => 915.0,
      'rateDetails' =>
-     [{ 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-26', 'rate1Person' => 450.0, 'rate2Person' => 450.0, 'rate3Person' => 495.0, 'rate4Person' => 540.0 },
-      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-27', 'rate1Person' => 437.0, 'rate2Person' => 450.0,
+     [{ 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-16', 'rate1Person' => 450.0, 'rate2Person' => 450.0, 'rate3Person' => 495.0, 'rate4Person' => 540.0 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-17', 'rate1Person' => 437.0, 'rate2Person' => 450.0,
         'rate3Person' => 495.0, 'rate4Person' => 540.0 },
-      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-28', 'rate1Person' => 477.0, 'rate2Person' => 450.0,
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-18', 'rate1Person' => 477.0, 'rate2Person' => 450.0,
         'rate3Person' => 495.0, 'rate4Person' => 540.0 },
-      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-30', 'rate1Person' => 455.0, 'rate2Person' => 455.0,
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-20', 'rate1Person' => 455.0, 'rate2Person' => 455.0,
+        'rate3Person' => 500.5, 'rate4Person' => 546.0 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-21', 'rate1Person' => 455.0, 'rate2Person' => 455.0,
+        'rate3Person' => 500.5, 'rate4Person' => 546.0 },
+      { 'rate1PersonTaxes' => 5.0, 'effectiveDate' => '2021-11-22', 'rate1Person' => 455.0, 'rate2Person' => 455.0,
         'rate3Person' => 500.5, 'rate4Person' => 546.0 }] }] }
 
 total2 = {}
@@ -65,7 +81,7 @@ user[:room_stays].map do |code|
     rate['rateDetails'].each do |room_date|
       next unless room_date['effectiveDate'] == code['date']
 
-      luffy = room_date['rate1Person'].to_f * code['rooms_requested'].to_f
+      puts luffy = room_date['rate1Person'].to_f * code['rooms_requested'].to_f
 
       if !total2[code['external_code']]
         total2[code['external_code']] = [{ dates: code['date'], sum: luffy }]
@@ -88,7 +104,9 @@ def totalAmount(total2)
       # pp el, idx
       acc[code] = [{ arrival: el[:dates], total_sum: el[:sum] }] unless acc[code]
       #     # p "----------------------"
-      next_day = total2[code][idx + 1] ? Date.parse(total2[code][idx + 1][:dates]).day : 0
+      # next_day = total2[code][idx + 1] ? Date.parse(total2[code][idx + 1][:dates]).day : 0
+      # hot fix, here i am really gettign the next day thre is no need to use index number
+      next_day = total2[code][idx] ? Date.parse(total2[code][idx][:dates]).day + 1 : 0
       # p next_day
       current_day = Date.parse(el[:dates]).day
       # p current_day
