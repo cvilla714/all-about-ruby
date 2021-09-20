76d2c26e3d91dd11a26e4664aa87b22193506826
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-20 13:40:45 -0400

    using the authentication module

---
+++ class6_using_authentication_module.rb
@@ -0,0 +1,14 @@
+system 'clear'
+
+require_relative 'class5_creating_module'
+
+users = [
+  { username: 'luffy', password: 'password1' },
+  { username: 'zorro', password: 'password2' },
+  { username: 'sanji', password: 'password3' },
+  { username: 'jinbei', password: 'password4' },
+  { username: 'yamato', password: 'password5' }
+]
+
+hashed_users = Crud.create_secure_users(users)
+puts hashed_users
